a8c2155b684d1dc234518733151a84d6799b8bd8
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2021-04-07 08:07:48 +1000

    Removing unused files

--- 10_flattenSolidityFiles.sh
+++ 10_flattenSolidityFiles.sh
@@ -9,8 +9,5 @@ echo "\$ rm -rf flattened/ ..."
 rm -rf flattened/
 echo "\$ mkdir flattened/ ..."
 mkdir flattened/
-echo "\$ truffle-flattener contracts/HoprDistributor.sol > flattened/HoprDistributor_flattened.sol ..."
+echo "\$ truffle-flattener contracts/token/ERC1155/presets/ERC1155PresetMinterPauser.sol > flattened/ERC1155PresetMinterPauser_flattened.sol ..."
 truffle-flattener contracts/token/ERC1155/presets/ERC1155PresetMinterPauser.sol > flattened/ERC1155PresetMinterPauser_flattened.sol
-
-
-# /Users/bok/Projects/NFT/contracts/token/ERC1155/presets/ERC1155PresetMinterPauser.sol
